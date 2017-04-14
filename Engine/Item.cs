b3a029cb88108e9b4607fa3cece3b60148af856b
Andreas-Grahn <andreas.grahn.1995@gmail.com>
2017-04-14 12:15:47 +0200

    Add project files.

---
+++ Engine/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Item
+    {
+
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public String NamePlural { get; set; }
+
+
+        public Item(int id, string name, string namePlural)
+        {
+            ID = id;
+            Name = name;
+            NamePlural = namePlural;
+        }
+
+
+    }
+}
